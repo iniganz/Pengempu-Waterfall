d7d013f92614da325576d711641b4a6ff55f24aa
iniganz <gandhiwulan123@gmail.com>
2026-01-14 09:13:08 +0800

    Fix undefined testimonials variable & add deployment guide

--- app/Http/Controllers/PublikController.php
+++ app/Http/Controllers/PublikController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Testimonial;
 
 class PublikController extends Controller
 {
- public function index()
-{
-    return view('publik.page.home', [
-        'title' => 'Index',
-    ]);
-}
+    public function index()
+    {
+        $testimonials = Testimonial::where('is_active', true)
+            ->latest()
+            ->get();
+
+        return view('publik.page.home', [
+            'title' => 'Index',
+            'testimonials' => $testimonials,
+        ]);
+    }
     public function contact()
     {
         return view('publik.page.contact', [
